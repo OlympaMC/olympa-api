13a2104254b0b9a1a75291b6b27e7f695e74d92f
Tristiisch <tristiisch74@hotmail.com>
2020-06-22 03:17:11 +0200

    ajout de bungee

--- src/java/fr/olympa/api/server/OlympaServer.java
+++ src/java/fr/olympa/api/server/OlympaServer.java
@@ -1,8 +1,9 @@
 package fr.olympa.api.server;
 
 public enum OlympaServer {
-
+	
 	ALL(null),
+	BUNGEE("Bungee", true),
 	AUTH("Auth", true),
 	LOBBY("Lobby", true),
 	DEV("Développement"),
@@ -11,25 +12,25 @@ public enum OlympaServer {
 	CREATIF("Créatif"),
 	LG("Loup-Garou", true),
 	PVPFAC("PvP-Factions");
-
+	
 	private final String name;
 	private final boolean multi;
-
+	
 	private OlympaServer(String name) {
 		this(name, false);
 	}
-
+	
 	private OlympaServer(String name, boolean multi) {
 		this.name = name;
 		this.multi = multi;
 	}
-
+	
 	public String getNameCaps() {
 		return name;
 	}
-
+	
 	public boolean hasMultiServers() {
 		return multi;
 	}
-
+	
 }
