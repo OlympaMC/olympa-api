4580c11ecdce270308e1940ac3ac5dc0d439cecf
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 07:46:46 +0200

    Ajout d'aliases pour les arguments ComplexCommand

--- src/java/fr/olympa/api/command/OlympaCommand.java
+++ src/java/fr/olympa/api/command/OlympaCommand.java
@@ -22,11 +22,11 @@ import fr.olympa.api.utils.Prefix;
 import net.md_5.bungee.api.chat.BaseComponent;
 
 public abstract class OlympaCommand {
-	
+
 	protected static Map<String, OlympaCommand> commandPreProcess = new HashMap<>();
 	protected static CommandMap cmap;
 	protected Plugin plugin;
-	protected List<String> alias;
+	protected List<String> aliases;
 	protected LinkedHashMap<Boolean, List<CommandArgument>> args = new LinkedHashMap<>();
 	protected String command;
 	protected String description;
@@ -37,45 +37,45 @@ public abstract class OlympaCommand {
 	protected Integer minArg = 0;
 	protected boolean allowConsole = true;
 	protected boolean isAsynchronous = false;
-	
-	public OlympaCommand(Plugin plugin, String command, OlympaPermission permission, String... alias) {
+
+	public OlympaCommand(Plugin plugin, String command, OlympaPermission permission, String... aliases) {
 		this.plugin = plugin;
 		this.command = command;
 		this.permission = permission;
-		this.alias = Arrays.asList(alias);
+		this.aliases = Arrays.asList(aliases);
 	}
-	
-	public OlympaCommand(Plugin plugin, String command, String... alias) {
+
+	public OlympaCommand(Plugin plugin, String command, String... aliases) {
 		this.plugin = plugin;
 		this.command = command;
-		this.alias = Arrays.asList(alias);
+		this.aliases = Arrays.asList(aliases);
 	}
-	
-	public OlympaCommand(Plugin plugin, String command, String description, OlympaPermission permission, String... alias) {
+
+	public OlympaCommand(Plugin plugin, String command, String description, OlympaPermission permission, String... aliases) {
 		this.plugin = plugin;
 		this.permission = permission;
 		this.command = command;
 		this.description = description;
-		this.alias = Arrays.asList(alias);
+		this.aliases = Arrays.asList(aliases);
 	}
-	
+
 	public void addCommandArguments(boolean isMandatory, List<CommandArgument> ca) {
 		args.put(isMandatory, ca);
 	}
-
+	
 	@Deprecated
 	public void addArgs(boolean isMandatory, List<String> arg) {
 		args.put(isMandatory, arg.stream().map(a -> new CommandArgument(a)).collect(Collectors.toList()));
 	}
-
+	
 	public void addArgs(boolean isMandatory, String... args) {
 		addCommandArguments(isMandatory, Arrays.stream(args).map(a -> new CommandArgument(a)).collect(Collectors.toList()));
 	}
-	
+
 	public String buildText(int min, String[] args) {
 		return String.join(" ", Arrays.copyOfRange(args, min, args.length));
 	}
-	
+
 	CommandMap getCommandMap() {
 		if (cmap == null)
 			try {
@@ -89,27 +89,27 @@ public abstract class OlympaCommand {
 			}
 		return cmap;
 	}
-	
+
 	public <T extends OlympaPlayer> T getOlympaPlayer() {
 		return AccountProvider.get(player.getUniqueId());
 	}
-	
+
 	public Player getPlayer() {
 		return player;
 	}
-	
+
 	public CommandSender getSender() {
 		return sender;
 	}
-	
+
 	public void setAllowConsole(boolean allowConsole) {
 		this.allowConsole = allowConsole;
 	}
-	
+
 	public boolean hasPermission() {
 		return this.hasPermission(permission);
 	}
-	
+
 	public boolean hasPermission(OlympaPermission perm) {
 		if (perm == null || player == null)
 			return true;
@@ -118,7 +118,7 @@ public abstract class OlympaCommand {
 			return false;
 		return perm.hasPermission(olympaPlayer);
 	}
-	
+
 	public boolean hasPermission(String permName) {
 		if (permName == null || permName.isEmpty())
 			return true;
@@ -127,11 +127,11 @@ public abstract class OlympaCommand {
 			return false;
 		return this.hasPermission(perm);
 	}
-	
+
 	public abstract boolean onCommand(CommandSender sender, Command cmd, String label, String[] args);
-	
+
 	public abstract List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args);
-	
+
 	private void build() {
 		usageString = args.entrySet().stream().map(entry -> {
 			boolean isMandatory = entry.getKey();
@@ -140,83 +140,83 @@ public abstract class OlympaCommand {
 		}).collect(Collectors.joining(" "));
 		minArg = (int) args.entrySet().stream().count();
 	}
-
+	
 	public void register() {
 		build();
 		ReflectCommand reflectCommand = new ReflectCommand(command);
-		if (alias != null)
-			reflectCommand.setAliases(alias);
+		if (aliases != null)
+			reflectCommand.setAliases(aliases);
 		if (description != null)
 			reflectCommand.setDescription(description);
 		reflectCommand.setExecutor(this);
 		getCommandMap().register("Olympa", reflectCommand);
 	}
-	
+
 	public void registerPreProcess() {
 		build();
 		commandPreProcess.put(command, this);
 	}
-	
+
 	public void sendDoNotHavePermission() {
 		sendError("Tu n'as pas la permission &l(◑_◑)");
 	}
-	
+
 	public void sendImpossibleWithConsole() {
 		sendError("Impossible avec la console.");
 	}
-	
+
 	public void sendImpossibleWithOlympaPlayer() {
 		sendError("Une erreur est survenu avec tes données.");
 	}
-	
+
 	public void sendIncorrectSyntax() {
 		sendError("Syntaxe incorrecte.");
 	}
-	
+
 	public void sendIncorrectSyntax(String correctSyntax) {
 		sendError("Syntaxe attendue : &o" + correctSyntax);
 	}
-	
+
 	public void sendUnknownPlayer(String name) {
 		sendError("Le joueur &4%s&c est introuvable.", name);
 		// TODO check historique player
 	}
-	
+
 	public void sendMessage(Iterable<? extends CommandSender> senders, Prefix prefix, String text, Object... args) {
 		text = prefix.formatMessage(text, args);
 		for (CommandSender sender : senders)
 			sender.sendMessage(text);
 	}
-	
+
 	public void broadcast(Prefix prefix, String text, Object... args) {
 		sendMessage(Bukkit.getOnlinePlayers(), prefix, text, args);
 	}
-	
+
 	public void broadcastToAll(Prefix prefix, String text, Object... args) {
 		broadcast(prefix, text, args);
 		prefix.sendMessage(Bukkit.getConsoleSender(), text, args);
 	}
-	
+
 	public void sendComponents(BaseComponent... components) {
 		sender.spigot().sendMessage(components);
 	}
-	
+
 	public void sendMessage(Prefix prefix, String message, Object... args) {
 		prefix.sendMessage(sender, message, args);
 	}
-	
+
 	public void sendInfo(String message, Object... args) {
 		sendMessage(Prefix.INFO, message, args);
 	}
-	
+
 	public void sendSuccess(String message, Object... args) {
 		sendMessage(Prefix.DEFAULT_GOOD, message, args);
 	}
-	
+
 	public void sendError(String message, Object... args) {
 		sendMessage(Prefix.DEFAULT_BAD, message, args);
 	}
-	
+
 	public void sendUsage(String label) {
 		sendMessage(Prefix.USAGE, "/%s %s", label, usageString);
 	}
--- src/java/fr/olympa/api/command/complex/ComplexCommand.java
+++ src/java/fr/olympa/api/command/complex/ComplexCommand.java
@@ -24,50 +24,59 @@ import fr.olympa.api.permission.OlympaPermission;
 import fr.olympa.core.spigot.OlympaCore;
 
 public class ComplexCommand extends OlympaCommand {
-
+	
 	private static final List<String> INTEGERS = Arrays.asList("1", "2", "3", "...");
 	private static final List<String> BOOLEAN = Arrays.asList("true", "false");
-
+	
 	public class InternalCommand {
 		public Cmd cmd;
 		public OlympaPermission perm;
 		public Method method;
 		public Object commands;
-
+		
 		InternalCommand(Cmd cmd, Method method, Object commandsClass) {
 			this.cmd = cmd;
-			this.perm = OlympaPermission.permissions.get(cmd.permissionName());
+			perm = OlympaPermission.permissions.get(cmd.permissionName());
 			this.method = method;
-			this.commands = commandsClass;
+			commands = commandsClass;
 		}
 	}
-
+	
 	private class ArgumentParser {
 		private Supplier<List<String>> tabArgumentsSupplier;
 		private Function<String, Object> supplyArgumentFunction;
-
+		
 		public ArgumentParser(Supplier<List<String>> tabArgumentsSupplier, Function<String, Object> supplyArgumentFunction) {
 			this.tabArgumentsSupplier = tabArgumentsSupplier;
 			this.supplyArgumentFunction = supplyArgumentFunction;
 		}
 	}
+	
+	public final Map<List<String>, InternalCommand> commands = new HashMap<>();
 
-	public final Map<String, InternalCommand> commands = new HashMap<>();
+	public InternalCommand getCommand(String argName) {
+		return commands.entrySet().stream().filter(entry -> entry.getKey().contains(argName.toLowerCase())).findFirst().map(entry -> entry.getValue()).orElse(null);
+	}
 
+	public boolean containsCommand(String argName) {
+		return commands.entrySet().stream().anyMatch(entry -> entry.getKey().contains(argName.toLowerCase()));
+	}
+	
 	private final Map<String, ArgumentParser> parsers = new HashMap<>();
-
+	
 	public ComplexCommand(Plugin plugin, String command, String description, OlympaPermission permission, String... alias) {
 		super(plugin, command, description, permission, alias);
-
+		
 		addArgumentParser("PLAYERS", () -> Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toList()), x -> {
 			Player result = Bukkit.getPlayerExact(x);
-			if (result == null) sendUnknownPlayer(x);
+			if (result == null)
+				sendUnknownPlayer(x);
 			return result;
 		});
 		addArgumentParser("INTEGER", () -> INTEGERS, x -> {
 			try {
 				return Integer.parseInt(x);
-			}catch (NumberFormatException e) {
+			} catch (NumberFormatException e) {
 				sendError(x + " doit être un nombre entier.");
 			}
 			return null;
@@ -75,146 +84,140 @@ public class ComplexCommand extends OlympaCommand {
 		addArgumentParser("DOUBLE", () -> Collections.EMPTY_LIST, x -> {
 			try {
 				return Double.parseDouble(x);
-			}catch (NumberFormatException e) {
+			} catch (NumberFormatException e) {
 				sendError(x + " doit être un nombre décimal.");
 			}
 			return null;
 		});
 		addArgumentParser("BOOLEAN", () -> BOOLEAN, Boolean::parseBoolean);
-		
-		this.registerCommandsClass(this);
-	}
 
+		registerCommandsClass(this);
+	}
+	
 	public void addArgumentParser(String name, Supplier<List<String>> tabArgumentsSupplier, Function<String, Object> supplyArgumentFunction) {
 		parsers.put(name, new ArgumentParser(tabArgumentsSupplier, supplyArgumentFunction));
 	}
-
+	
 	public boolean noArguments(CommandSender sender) {
 		return false;
 	}
-
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length == 0) {
-			if (!noArguments(sender)) this.sendIncorrectSyntax();
+			if (!noArguments(sender))
+				this.sendIncorrectSyntax();
 			return true;
 		}
-
-		InternalCommand internal = this.commands.get(args[0].toLowerCase());
+		
+		InternalCommand internal = getCommand(args[0]);
 		if (internal == null) {
-			this.sendError("La commande n'existe pas.");
+			sendError("La commande n'existe pas.");
 			return true;
 		}
-
+		
 		Cmd cmd = internal.cmd;
 		if (cmd.player() && !(sender instanceof Player)) {
-			this.sendImpossibleWithConsole();
+			sendImpossibleWithConsole();
 			return true;
 		}
-
+		
 		if (!cmd.permissionName().isEmpty() && !internal.perm.hasSenderPermission(sender)) {
-			this.sendDoNotHavePermission();
+			sendDoNotHavePermission();
 			return true;
 		}
-
+		
 		if (args.length - 1 < cmd.min()) {
-			if ("".equals(cmd.syntax())) {
+			if ("".equals(cmd.syntax()))
 				this.sendIncorrectSyntax();
-			} else {
+			else
 				this.sendIncorrectSyntax(internal.method.getName() + " " + cmd.syntax());
-			}
 			return true;
 		}
-
+		
 		Object[] argsCmd = new Object[args.length - 1];
 		for (int i = 1; i < args.length; i++) {
 			String arg = args[i];
 			String type = i > cmd.args().length ? "" : cmd.args()[i - 1];
 			Object result = null;
 			ArgumentParser parser = parsers.get(type);
-			if (parser != null) {
+			if (parser != null)
 				result = parser.supplyArgumentFunction.apply(arg);
-			} else {
+			else
 				result = arg;
-			}
-			if (result == null) return true;
+			if (result == null)
+				return true;
 			argsCmd[i - 1] = result;
 		}
-
+		
 		try {
 			internal.method.invoke(internal.commands, new CommandContext(this, argsCmd, label));
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			this.sendError("Une erreur est survenue.");
+			sendError("Une erreur est survenue.");
 			e.printStackTrace();
 		}
-
+		
 		return true;
 	}
-
+	
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
 		List<String> tmp = new ArrayList<>();
 		List<String> find = new ArrayList<>();
 		String sel = args[0];
-
-		if (args.length == 1) {
-			for (Entry<String, InternalCommand> en : this.commands.entrySet()) { // PERMISSIONS
+		
+		if (args.length == 1)
+			for (Entry<List<String>, InternalCommand> en : commands.entrySet()) { // PERMISSIONS
 				String perm = en.getValue().cmd.permissionName();
-				if (perm != null && this.hasPermission(perm)) {
-					find.add(en.getKey());
-				}
+				if (perm != null && this.hasPermission(perm))
+					find.add(en.getKey().get(0));
 			}
-		} else if (args.length >= 2) {
+		else if (args.length >= 2) {
 			int index = args.length - 2;
-			if (!this.commands.containsKey(sel)) {
+			if (!containsCommand(sel))
 				return tmp;
-			}
-			InternalCommand internal = this.commands.get(sel);
+			InternalCommand internal = getCommand(sel);
 			String[] needed = internal.cmd.args();
-			if (needed.length <= index) {
+			if (needed.length <= index)
 				return tmp;
-			}
-			if (!internal.cmd.permissionName().isEmpty() && !internal.perm.hasSenderPermission(sender)) {
+			if (!internal.cmd.permissionName().isEmpty() && !internal.perm.hasSenderPermission(sender))
 				return tmp;
-			}
 			sel = args[index + 1];
 			String type = needed[index];
 			ArgumentParser parser = parsers.get(type);
-			if (parser != null) {
+			if (parser != null)
 				find.addAll(parser.tabArgumentsSupplier.get());
-			} else {
+			else
 				find.addAll(Arrays.asList(type.split("\\|")));
-			}
-		} else {
+		} else
 			return tmp;
-		}
-
-		for (String arg : find) {
-			if (arg.startsWith(sel)) {
+		
+		for (String arg : find)
+			if (arg.startsWith(sel))
 				tmp.add(arg);
-			}
-		}
 		return tmp;
 	}
-
+	
 	/**
 	 * Register all available commands from an instance of a Class
 	 * @param commandsClassInstance Instance of the Class
 	 */
 	public void registerCommandsClass(Object commandsClassInstance) {
-		for (Method method : commandsClassInstance.getClass().getDeclaredMethods()) {
+		for (Method method : commandsClassInstance.getClass().getDeclaredMethods())
 			if (method.isAnnotationPresent(Cmd.class)) {
 				Cmd cmd = method.getDeclaredAnnotation(Cmd.class);
-				if (method.getParameterCount() == 1) {
+				if (method.getParameterCount() == 1)
 					if (method.getParameterTypes()[0] == CommandContext.class) {
-						this.commands.put(method.getName().toLowerCase(), new InternalCommand(cmd, method, commandsClassInstance));
+						List<String> argNames = new ArrayList<>();
+						argNames.add(method.getName().toLowerCase());
+						if (cmd.aliases() != null)
+							argNames.addAll(Arrays.asList(cmd.aliases()));
+						commands.put(argNames, new InternalCommand(cmd, method, commandsClassInstance));
 						continue;
 					}
-				}
 				OlympaCore.getInstance()
 						.sendMessage("Error when loading command annotated method " + method.getName() + " in class " + commandsClassInstance.getClass().getName() + ". Required argument: fr.olympa.api.command.complex.CommandContext");
 			}
-		}
 	}
-
+	
 }
