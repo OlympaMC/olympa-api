d43230daf331e53210374e807f380c339909fb22
SkytAsul <skytasul@gmail.com>
2020-07-04 17:24:28 +0200

    Fixes holograms, player informations, add fish flag

--- src/java/fr/olympa/api/player/OlympaPlayerInformations.java
+++ src/java/fr/olympa/api/player/OlympaPlayerInformations.java
@@ -2,15 +2,11 @@ package fr.olympa.api.player;
 
 import java.util.UUID;
 
-import fr.olympa.api.clans.ClanPlayerData;
-
 public interface OlympaPlayerInformations {
 	
 	long getId();
 	
 	String getName();
-
-	ClanPlayerData getClanData();
 	
 	UUID getUUID();
 	
