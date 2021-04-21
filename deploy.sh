b5937263e0d0ecfa83d5f436b80d069780a983c5
Tristiisch <tristiisch74@hotmail.com>
2021-04-21 20:46:07 +0200

    up script

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,14 @@
 ## Script to deploy api with good version
 ## Author > Tristiisch
 #
+# ./deploy.sh date "2021-02-26 18:30:00"
+# ./deploy.sh master
+# ./deploy.sh dev
+#
 
+# PARAMETRES
+PLUGIN_NAME="api"
 USE_BRANCH="master"
-
 ACTUAL_COMMIT_ID=`cat target/commitId`
 
 if [ -n "$1" ]; then
@@ -25,7 +30,19 @@ if [ -n "$1" ]; then
 else
 	echo -e "\e[0;36mTu peux choisir la version du core en ajoutant une date (ex './deploy.sh date \"2021-02-26 18:30:00\"') ou une branch (ex './deploy.sh dev').\e[0m"
 fi
-
+git pull --all
+if [ "$?" -ne 0 ]; then
+	echo -e "\e[91mEchec du git pull ! Regarde les conflits. Code erreur $rc\e[0m"
+	echo -e "\e[91mTentative de git reset\e[0m"
+	git reset --hard HEAD
+	if [ "$?" -ne 0 ]; then
+		echo -e "\e[91mEchec du git reset ! Code erreur $rc\e[0m"; exit $rc
+	fi
+	git pull --all
+	if [ "$?" -ne 0 ]; then
+		echo -e "\e[91mEchec du git pull ! Regarde les conflits. Code erreur $rc\e[0m"; exit $rc
+	fi
+fi
 if [ -n "$BRANCH_NAME" ]; then
 	exists=`git show-ref refs/heads/$BRANCH_NAME`
 	if [ -n "$exists" ]; then
@@ -39,25 +56,22 @@ if [ -n "$BRANCH_NAME" ]; then
 	#	unset BRANCH_NAME
 	#fi
 fi
-
 if [ -n "$DATE" ]; then
 	git checkout 'master@{$DATE}' --force
 elif [ -z "$BRANCH_NAME" ]; then
 	git reset --hard HEAD
 	git checkout master --force
 fi
-
 if [ -n "$ACTUAL_COMMIT_ID" ]; then
-	git pull
 	if [ "$ACTUAL_COMMIT_ID" = `git rev-parse HEAD` ]; then
-		echo -e "\e[32mPas besoin de maven install l'api, le jar est déjà crée.\e[0m"
+		echo -e "\e[32mPas besoin de maven install l'$PLUGIN_NAME, le jar est déjà crée.\e[0m"
 		exit 0
 	fi
 fi
-git pull && mvn install
+mvn install
 if [ "$?" -ne 0 ]; then
-	echo -e "\e[91m\n\nLe build de l'api a échoué !\e[0m"; exit $rc
+	echo -e "\e[91m\n\nLe build de l'$PLUGIN_NAME a échoué !\e[0m"; exit $rc
 else
 	echo `git rev-parse HEAD` > target/commitId
 fi
-echo -e "\e[32mLe jar du commit de l'api $(cat target/commitId) a été crée.\e[0m"
+echo -e "\e[32mLe jar du commit de l'$PLUGIN_NAME $(cat target/commitId) a été crée.\e[0m"
