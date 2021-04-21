33a047e5ce17b21671c6906548064a8e08d6fc51
Tristiisch <tristiisch74@hotmail.com>
2021-04-21 21:52:13 +0200

    remove async kick on stop

--- src/java/fr/olympa/api/plugin/OlympaSpigot.java
+++ src/java/fr/olympa/api/plugin/OlympaSpigot.java
@@ -93,17 +93,15 @@ public abstract class OlympaSpigot extends OlympaAPIPlugin implements OlympaCore
 	@Override
 	public void onDisable() {
 		super.onDisable();
-		new Thread(() -> {
-			while (!Bukkit.getOnlinePlayers().isEmpty()) {
-				for (Player p : Bukkit.getOnlinePlayers())
-					p.kickPlayer("Server closed");
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+		while (!Bukkit.getOnlinePlayers().isEmpty()) {
+			for (Player p : Bukkit.getOnlinePlayers())
+				p.kickPlayer("Server closed");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		}).start();
+		}
 	}
 
 	@Override
