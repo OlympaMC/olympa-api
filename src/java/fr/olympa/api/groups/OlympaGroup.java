dbdab0d46bd3c84fc4da15424d08ca6b308ae916
Bullobily <bullobily@gmail.com>
2021-04-24 09:15:44 +0200

    up créa groups

--- src/java/fr/olympa/api/groups/OlympaGroup.java
+++ src/java/fr/olympa/api/groups/OlympaGroup.java
@@ -31,16 +31,16 @@ public enum OlympaGroup {
 	BUILDER(14, 40, OlympaServer.ALL, "Buildeur", "Buildeuse", "&2%s ", ":&r", false),
 	ANIMATOR(13, 38, OlympaServer.ALL, "Animateur", "Animatrice", "&d%s ", ":&r", false),
 	GRAPHISTE(15, 36, OlympaServer.ALL, "Graphiste", "Graphiste", "&b%s ", ":&r", false),
-	FRIEND(16, 34, OlympaServer.ALL, "Ami", "Amie", "&e%s ", ":&r", false),
+	FRIEND(16, 34, OlympaServer.ALL, "Ami", "Amie", "&e%s ", ":&r ", false),
 	YOUTUBER(17, 32, OlympaServer.ALL, "Youtubeur", "Youtubeuse", "&5%s ", ":&r", false),
 	MINI_YOUTUBER(18, 30, OlympaServer.ALL, "M-Youtubeur", "M-Youtubeuse", "&d%s ", ":&r", false),
 
 	VIP(19, 20, OlympaServer.ALL, "VIP", "VIP", "&6%s", ":&r", false),
 
-	CREA_CONSTRUCTOR(22, 1, OlympaServer.CREATIF, "Constructeur", "Constructrice", "&9%s ", ":&r", false),
-	CREA_ARCHITECT(23, 2, OlympaServer.CREATIF, "Architecte", "Architecte", "&e%s ", ":&r", false),
-	CREA_CREATOR(24, 3, OlympaServer.CREATIF, "Créateur", "Créatrice", "&6%s ", ":&r", false),
-
+	CREA_CREATOR(24, 23, OlympaServer.CREATIF, "Créateur", "Créatrice", "&3%s ", ":&r", false),
+	CREA_ARCHITECT(23, 22, OlympaServer.CREATIF, "Architecte", "Architecte", "&6%s ", ":&r", false),
+	CREA_CONSTRUCTOR(22, 21, OlympaServer.CREATIF, "Bâtisseur", "Bâtisseuse", "&e%s ", ":&r", false),
+													
 	PLAYER(20, 0, OlympaServer.ALL, "Joueur", "Joueuse", "&7 ", ":", false);
 
 	//ZTA_MAFIEUX(21, 10, OlympaServer.ZTA, "Mafieux", "Mafieuse", "&d%s ", ":&r", false),
