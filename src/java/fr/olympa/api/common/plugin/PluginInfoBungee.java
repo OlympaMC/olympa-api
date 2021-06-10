1b01977062d8a12c314a1455b335f2a1f9fb5652
Tristiisch <tristiisch74@hotmail.com>
2021-06-10 08:49:15 +0200

    fix author empty in bungee.yml for plugins

--- src/java/fr/olympa/api/common/plugin/PluginInfoBungee.java
+++ src/java/fr/olympa/api/common/plugin/PluginInfoBungee.java
@@ -3,6 +3,7 @@ package fr.olympa.api.common.plugin;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import net.md_5.bungee.api.plugin.Plugin;
@@ -17,11 +18,16 @@ public class PluginInfoBungee extends PluginInfoAdvanced {
 		SuperVersion supVersion = new SuperVersion(desc.getVersion());
 		superVersion = supVersion;
 
-		authors = Arrays.asList(desc.getAuthor().split(" *, *"));
+		if (desc.getAuthor() == null)
+			authors = new ArrayList<>();
+		else
+			authors = Arrays.asList(desc.getAuthor().split(" *, *"));
 		description = desc.getDescription();
 		//		apiVersion = desc.getAPIVersion();
 		//		provides = desc.getProvides();
+		provides = new ArrayList<>();
 		//		contributors = desc.getContributors();
+		contributors = new ArrayList<>();
 		//		website = desc.getWebsite();
 		//		enabled = plugin.isEnabled();
 		//		if (!enabled) {
