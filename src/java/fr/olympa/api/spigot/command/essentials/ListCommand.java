e13a11c5483f0392d31a8dc6080cc694a848ee7f
Tristiisch <tristiisch74@hotmail.com>
2021-08-17 07:53:53 +0200

    remove vanished players for online count in /list

--- src/java/fr/olympa/api/spigot/command/essentials/ListCommand.java
+++ src/java/fr/olympa/api/spigot/command/essentials/ListCommand.java
@@ -15,11 +15,13 @@ import org.bukkit.plugin.Plugin;
 
 import fr.olympa.api.common.groups.OlympaGroup;
 import fr.olympa.api.common.permission.OlympaSpigotPermission;
+import fr.olympa.api.common.permission.list.OlympaAPIPermissionsSpigot;
 import fr.olympa.api.common.player.Gender;
 import fr.olympa.api.common.player.OlympaPlayer;
 import fr.olympa.api.common.provider.AccountProviderAPI;
 import fr.olympa.api.spigot.afk.AfkHandler;
 import fr.olympa.api.spigot.command.OlympaCommand;
+import fr.olympa.api.spigot.vanish.IVanishApi;
 import fr.olympa.core.spigot.OlympaCore;
 
 public class ListCommand extends OlympaCommand {
@@ -31,19 +33,24 @@ public class ListCommand extends OlympaCommand {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Collection<? extends Player> players = Bukkit.getOnlinePlayers();
-		sendSuccess("Il y a §2%s§a joueur%s en ligne:", players.size(), players.size() == 1 ? "" : "s");
+		boolean canSeeVanish = hasPermission(OlympaAPIPermissionsSpigot.VANISH_SEE);
 		Map<OlympaGroup, List<Player>> groups = new EnumMap<>(OlympaGroup.class);
+		OlympaCore core = OlympaCore.getInstance();
+		AfkHandler afk = core.getAfkHandler();
+		IVanishApi vanish = core.getVanishApi();
+		int onlineCount = 0;
 		for (Player p : players) {
-			if (OlympaCore.getInstance().getVanishApi().isVanished(p))
+			if (!canSeeVanish && vanish.isVanished(p))
 				continue;
 			OlympaPlayer player = AccountProviderAPI.getter().get(p.getUniqueId());
 			List<Player> groupPlayers = groups.computeIfAbsent(player.getGroup(), group -> new ArrayList<>());
 			groupPlayers.add(p);
+			onlineCount++;
 		}
-		AfkHandler afk = OlympaCore.getInstance().getAfkHandler();
+		sendSuccess("Il y a §2%s§a joueur%s en ligne:", onlineCount, onlineCount == 1 ? "" : "s");
 		groups.entrySet().stream().sorted((o1, o2) -> Integer.compare(o1.getKey().ordinal(), o2.getKey().ordinal())).forEach(entry -> {
 			sender.sendMessage("§7➤ " + entry.getKey().getColor() + entry.getKey().getName(Gender.UNSPECIFIED) + "§8 (" + entry.getValue().size() + "): §7"
-					+ entry.getValue().stream().map(x -> x.getName() + (afk != null && afk.isAfk(x) ? "§8 (AFK)§7" : "")).collect(Collectors.joining(", ")));
+					+ entry.getValue().stream().map(x -> x.getName() + (afk != null && afk.isAfk(x) ? "§8 (AFK)§7" : "") + (canSeeVanish && vanish != null && vanish.isVanished(x) ? "§8 (VANISH)§7" : "")).collect(Collectors.joining(", ")));
 		});
 		return false;
 	}
