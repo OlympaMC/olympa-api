a46d4ccb0350a04ce72e0f87ec375615fe85a53a
Tristiisch <tristiisch74@hotmail.com>
2021-08-26 00:14:47 +0200

    remove debug msg, gui is totally fix

--- src/java/fr/olympa/api/spigot/gui/Inventories.java
+++ src/java/fr/olympa/api/spigot/gui/Inventories.java
@@ -20,7 +20,9 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import fr.olympa.api.utils.Prefix;
 
@@ -73,9 +75,44 @@ public class Inventories implements Listener {
 					return gui;
 		return null;
 	}
-
+	
 	public static boolean isInSystem(Player p) {
 		return g.containsKey(p);
+	}
+	
+	public static Map<Integer, ItemStack> getDoubleClickItems(Player p, ItemStack itemAsk, int slot, @NotNull InventoryView inventoryView) {
+		int maxSize = itemAsk.getMaxStackSize();
+		int maxGetSize = maxSize - itemAsk.getAmount();
+		if (maxGetSize <= 0)
+			return null;
+		Inventory guiInventory = inventoryView.getTopInventory();
+		Inventory playerInventory = inventoryView.getBottomInventory();
+		int maxInvSize = guiInventory.getSize() + playerInventory.getSize();
+		Map<Integer, ItemStack> itemGet = new HashMap<>();
+		for (int i = 0; i < maxInvSize && maxGetSize > 0; i++) {
+			@Nullable
+			ItemStack item = inventoryView.getItem(i);
+			if (item == null) continue;
+			if (!item.isSimilar(itemAsk)) continue;
+			ItemStack newItem ;
+			if (item.getAmount() > maxGetSize) {
+				newItem = new ItemStack(item);
+				newItem.setAmount(maxGetSize);
+			} else {
+				newItem = item;
+			}
+			maxGetSize -= item.getAmount();
+			itemGet.put(i, item);
+		}
+		return itemGet;
+	}
+
+	// TODO finish execute double click item
+	public static void getDoubleClickItems(Player p, ItemStack itemAsk, Map<Integer, ItemStack> itemToSetInCusor, @NotNull InventoryView inventoryView) {
+		@NotNull
+		ItemStack cursor = p.getItemOnCursor();
+		if (itemAsk.isSimilar(p.getItemOnCursor()))
+		p.getItemOnCursor();
 	}
 
 	@EventHandler
@@ -91,20 +128,28 @@ public class Inventories implements Listener {
 		ItemStack current = event.getCurrentItem();
 		@Nullable
 		ItemStack cursor = event.getCursor();
+//		System.out.print(String.format("Debug Info Gui Error : player > %s current > %s cursor > %s click > %s action > %s isFromInv > %s", p.getName(),
+//				current != null ? current.getType().name() : "null",current != null ? current.getType().name() : "null",  event.getClick().name(), event.getAction().name(), inv == p.getInventory()));
 		try {
 			OlympaGUI gui = getGUI(event.getView().getTopInventory());
 			if (gui == null)
 				return;
-			if (event.getClick() == ClickType.DOUBLE_CLICK && gui.noDoubleClick()) {
-				event.setCancelled(true);
-				return;
+			if (event.getClick() == ClickType.DOUBLE_CLICK) {
+				if (gui.noDoubleClick()) {
+					event.setCancelled(true);
+					return;
+				}
+//				if (cursor == null) {
+//					getDoubleClickItems(p, current, event.getSlot(), event.getView());
+//				}
 			}
+			boolean isShiftClic = event.isShiftClick() && current != null && current.getType() != Material.AIR;
 			if (inv == p.getInventory()) {
-				if (event.isShiftClick() && current != null && current.getType() != Material.AIR)
+				if (isShiftClic)
 					event.setCancelled(gui.onMoveItem(p, current, true, event.getSlot()));
 				return;
 			}
-			if (event.isShiftClick() && current != null && current.getType() != Material.AIR && gui.onMoveItem(p, current, false, event.getSlot())) {
+			if (isShiftClic && gui.onMoveItem(p, current, false, event.getSlot())) {
 				event.setCancelled(true);
 				return;
 			}
